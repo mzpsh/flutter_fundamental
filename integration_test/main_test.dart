ca6cf1b70a1703b2c03a938178821f540b30bc58
mirzamiftahulfadillah@gmail.com <mirzamiftahulfadillah@gmail.com>
2022-03-24 16:54:42 +0800

    third submission, utc time fix, additional notifications tests

--- integration_test/main_test.dart
+++ integration_test/main_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-
+import 'package:flutter_fundamental/utils/daily_notification.dart';
 import 'package:flutter_fundamental/main.dart' as app;
 
 void main() {
@@ -15,5 +15,46 @@ void main() {
 
     expect(find.text('Melting Pot'), findsOneWidget);
     expect(find.text('Kafe Kita'), findsOneWidget);
+  });
+
+  testWidgets('Able to set scheduled notification.',
+      (WidgetTester tester) async {
+    app.main();
+    await tester.pumpAndSettle();
+    await tester.pump(const Duration(seconds: 3));
+
+    await tester.tap(find.text('Settings'));
+    await tester.pump(const Duration(seconds: 1));
+
+    await tester.tap(find.text('Daily restaurant reminder'));
+    await tester.pump(const Duration(seconds: 3));
+
+    final notificationRequests =
+        await flutterLocalNotificationsPlugin.pendingNotificationRequests();
+
+    expect(notificationRequests.isNotEmpty, true);
+  });
+
+  testWidgets(
+      'Able to set scheduled notification and cancel all notifications.',
+      (WidgetTester tester) async {
+    app.main();
+    await tester.pumpAndSettle();
+    await tester.pump(const Duration(seconds: 3));
+
+    await tester.tap(find.text('Settings'));
+    await tester.pump(const Duration(seconds: 1));
+
+    await tester.tap(find.text('Daily restaurant reminder'));
+    await tester.pump(const Duration(seconds: 5));
+
+    // Cancelling the notifications
+    await tester.tap(find.text('Daily restaurant reminder'));
+    await tester.pump(const Duration(seconds: 1));
+
+    final notificationRequests =
+        await flutterLocalNotificationsPlugin.pendingNotificationRequests();
+
+    expect(notificationRequests.isEmpty, true);
   });
 }
